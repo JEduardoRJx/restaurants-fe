e5344d2f38118bcf155f9a304d1c50eb5dd3f875
Eduardo <jeduardorjx@gmail.com>
2020-10-17 12:57:48 -0600

    log restaurants in componentdidmount

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component, useState} from 'react';
 import './App.scss';
+import { getRestaurants } from './apiCalls';
 
 export class App extends Component {
+
+  async componentDidMount() {
+    try {
+      const restaurants = await getRestaurants();
+      console.log('ayyyyo', restaurants)
+    } catch {
+      console.log("can't fetch restaurants")
+    }
+  }
 
   render() {
     return (
