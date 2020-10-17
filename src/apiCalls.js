62cd2592032bb05a1f275c1dd678f314c37c7529
Eduardo <jeduardorjx@gmail.com>
2020-10-17 12:57:11 -0600

    fetch all restaurants

---
+++ src/apiCalls.js
@@ -0,0 +1,15 @@
+const baseUrl = "https://code-challenge.spectrumtoolbox.com/api/restaurants";
+const headers = {
+  headers: {
+    Authorization: "Api-Key q3MNxtfep8Gt",
+    }
+}
+
+export const getRestaurants = async () => {
+  const res = await fetch(baseUrl, headers);
+  if (!res.ok) {
+    throw Error ("Restaurants are closed, can't fetch restaurants");
+  }
+  const restaurants = await res.json();
+  return restaurants;
+}
