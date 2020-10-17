4e7719f91228d9228a2f2b72a25a7db45f1dac65
Eduardo <jeduardorjx@gmail.com>
2020-10-17 12:25:15 -0600

    change app to a class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React, { Component, useState} from 'react';
 import './App.scss';
 
+export class App extends Component {
+
+  render() {
+    return (
+      <main>
+        Hello
+      </main>
+    )
+  }
+}
 
 export default App;
