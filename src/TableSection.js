948f945ba4dfc5e4c35bf7eedaf361ab957e8516
Eduardo <jeduardorjx@gmail.com>
2020-10-18 13:33:03 -0600

    create function to iterate and render restaurant items

--- src/TableSection.js
+++ src/TableSection.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { TableItem} from './TableItem'
+import { TableItem } from './TableItem';
+import { restaurants } from './restaurants';
 
 export const TableSection = () => {
+  
+  const renderRestaurants = (restaurants) => {
+      return restaurants.map(rest => <TableItem  rest={rest} />)
+  }
+  
+  
+
   return (
     <section className="section has-background-primary">
       <h1 className="title">Section</h1>
-
+      {renderRestaurants(restaurants)}
       
       {/* <h2 className="subtitle">
         A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
       </h2> */}
-      <TableItem />
     </section>
   )
 }
