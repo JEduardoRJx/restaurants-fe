1115ad1c5955ac49ce5110c5639d715227b13b2e
Eduardo <jeduardorjx@gmail.com>
2020-10-18 14:15:04 -0600

    Create function to handle change of text and add to state

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Search = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchText = e => {
+    setSearchText(e.target.value)
+  }
+
+  // handleSearchClick
+  // Here you compare the search item with all the restaurants
+  // then you send the filtered restuarnts back up into app
+  // You set them to state there
+
   return (
-    <section className="section has-background-primary">
-      Search
+    <section className="section has-background-link is-flex">
+      <input className="input" type="text" placeholder="Search"
+        onChange={e => handleSearchText(e)} />
+      <button className="button is-danger">Search</button>
+      <p>{searchText}</p>
     </section>
   )
 }
