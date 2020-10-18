229a9b5728039b5d92302f39e4771844f9839b90
Eduardo <jeduardorjx@gmail.com>
2020-10-18 13:49:39 -0600

    import mock data and set it to state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { useState , useEffect} from 'react';
 import './App.scss';
 import { getRestaurants } from './apiCalls';
-import { TableSection }  from './TableSection'
+import { TableSection }  from './TableSection';
+import { restaurants } from './restaurants';
+
 
 
 export const App = () => {
   const [allRestaurants, setAllRestaurants] = useState([]);
 
-  // useEffect( () => {
+  useEffect( () => {
   //   getRestaurants()
   //     .then(data => setAllRestaurants(data))
-  // }, []);
+    setAllRestaurants(restaurants)
+
+  }, []);
 
     return (
       <main className="container is-widescreen">
-        {console.log(allRestaurants)}
-        Hello
-        <TableSection />
+        {/* {console.log(allRestaurants)} */}
+        <TableSection restaurants={allRestaurants} />
       </main>
     )
 }
