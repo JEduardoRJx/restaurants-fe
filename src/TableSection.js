034f3c7ff46ccbd95b7f7ecb41b8ce6be8e497c1
Eduardo <jeduardorjx@gmail.com>
2020-10-18 12:03:57 -0600

    create a teble section component

---
+++ src/TableSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TableItem} from './TableItem'
+
+export const TableSection = () => {
+  return (
+    <section className="section has-background-primary">
+
+  </section>
+  )
+}
