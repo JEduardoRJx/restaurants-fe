dcb562e3bd84c8a321f858fe2379dd1df506eab6
Eduardo <jeduardorjx@gmail.com>
2020-10-18 15:59:54 -0600

    Add e to functions and remove comments

--- src/Search.js
+++ src/Search.js
@@ -3,16 +3,11 @@ import React, { useState } from 'react';
 export const Search = ({ restaurants, setFilteredRestaurants }) => {
   const [searchText, setSearchText] = useState('');
 
-  const handleSearchText = e => {
+  const handleSearchText = (e) => {
     setSearchText(e.target.value)
   }
 
-  // handleSearchClick
-  // Here you compare the search item with all the restaurants
-  // then you send the filtered restuarnts back up into app
-  // You set them to state there
-
-  const handleSearchClick = (e) => {
+  const handleSearch = (e) => {
     if (e.key === 'Enter' || e.target.type === 'submit') {
       searchRestaurants(searchText)
     }
@@ -24,20 +19,18 @@ export const Search = ({ restaurants, setFilteredRestaurants }) => {
       const lowerCaseName = rest.name.toLowerCase();
       return lowerCaseName.includes(lowerCaseText)
     })
-    // return filteredRestaurants
-    // Here is where you iterate to finde the restaruants that match the name
-    // then send that array back to app
-    // then you set it to state
-    setFilteredRestaurants(filteredRestaurants);
+          setFilteredRestaurants(filteredRestaurants);
   }
 
   return (
     <section className="section has-background-link is-flex">
       <input className="input" type="text" placeholder="Search"
         onChange={e => handleSearchText(e)} 
-        onKeyPress={(e) => handleSearchClick(e)} />
+        onKeyPress={(e) => handleSearch(e)} 
+        />
       <button className="button is-danger"
-        onClick={(e) => handleSearchClick(e)}>Search</button>
+        onClick={(e) => handleSearch(e)}
+        >Search</button>
       <p>{searchText}</p>
     </section>
   )
