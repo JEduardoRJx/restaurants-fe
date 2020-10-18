3d360064e535333fa324776ecb2ac51cdd962d95
Eduardo <jeduardorjx@gmail.com>
2020-10-18 16:19:18 -0600

    Basic styles and layout for filters component

--- src/Filters.js
+++ src/Filters.js
@@ -3,7 +3,28 @@ import React from 'react';
 export const Filters = () => {
 
   return (
-    <section className="section has-background-secondary">
+    <section className="section has-background-success">
+      <p>Filters</p>
+
+      <div className="container is-flex">
+        <div className="container">
+          <p>State</p>
+          <div className="select">
+            <select>
+              <option>All</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="container">
+          <p>Genre</p>
+          <div className="select">
+            <select>
+              <option>All</option>
+            </select>
+          </div>
+        </div>
+      </div>
 
     </section>
   )
