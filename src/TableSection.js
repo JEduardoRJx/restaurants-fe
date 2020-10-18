a40417074523521fd7794b5e29df74fff0a43ecd
Eduardo <jeduardorjx@gmail.com>
2020-10-18 12:04:45 -0600

    Create basic section layout and import table item plus calling it

--- src/TableSection.js
+++ src/TableSection.js
@@ -4,7 +4,13 @@ import { TableItem} from './TableItem'
 export const TableSection = () => {
   return (
     <section className="section has-background-primary">
+      <h1 className="title">Section</h1>
 
-  </section>
+      
+      {/* <h2 className="subtitle">
+        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+      </h2> */}
+      <TableItem />
+    </section>
   )
 }
