8092a96174b57aada040c5f5390047ef648e552f
Eduardo <jeduardorjx@gmail.com>
2020-10-19 14:07:47 -0600

    Only bring in setsearchtext from props

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 
-export const Search = ({setSearchText, handleSearch }) => {
+export const Search = ({ setSearchText }) => {
+  const [text, setText] = useState('');
 
-  const handleSearchText = (e) => {
-    setSearchText(e.target.value)
+  const handleText = (e) => {
+    setText(e.target.value)
   }
 
+  const handleSearch = (e) => {
+    console.log('text', text)
+    if (e.key === 'Enter' || e.target.type === 'submit') {
+      setSearchText(text)
+    }
+  }
 
   // const searchRestaurants = (text) => {
   //   const lowerCaseText = text.toLowerCase();
@@ -19,13 +26,12 @@ export const Search = ({setSearchText, handleSearch }) => {
   return (
     <section className="section has-background-link is-flex">
       <input className="input" type="text" placeholder="Search"
-        onChange={e => handleSearchText(e)} 
+        onChange={e => handleText (e)} 
         onKeyPress={(e) => handleSearch(e)} 
         />
       <button className="button is-danger"
         onClick={(e) => handleSearch(e)}
         >Search</button>
-      <p>{searchText}</p>
     </section>
   )
 }
