fcf05de46f129ee075fa9818e0fab0f4d8527a0b
Eduardo <jeduardorjx@gmail.com>
2020-10-19 16:35:10 -0600

    create isloading state variable, update filteredrestaurants and isloading on mount

--- src/App.js
+++ src/App.js
@@ -12,28 +12,45 @@ export const App = () => {
   const [allRestaurants, setAllRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([])
   const [searchText, setSearchText] = useState('');
-  const [state, setState] = useState('')
-  const [genre, setGenre] = useState('')
+  const [state, setState] = useState('');
+  const [genre, setGenre] = useState('');
+  const [isLoading, setLoading] = useState(true)
 
   useEffect( () => {
   //   getRestaurants()
   //     .then(data => setAllRestaurants(data))
     setAllRestaurants(restaurants)
+    setFilteredRestaurants(restaurants)
+    setLoading(false)
   }, []);
 
   useEffect(() => {
-    console.log('searchtext', searchText);
-    const restaurants = allRestaurants.filter(rest => rest.genre.includes(genre) &&
-      (rest.state === state || state === ''))
-      console.log('rest', restaurants)
-      // setFilteredRestaurants(restaurants);
-  }, [state, genre, searchText]);
+      const restaurants = searchRestaurants();
+      setFilteredRestaurants(restaurants);
+  }, [state, genre, searchText, isLoading]);
+
+  const searchRestaurants = () => {
+    // let restaurants = allRestaurants.filter(rest => {
+    //   const lowerCaseSearchText = searchText.toLowerCase();
+    //   const lowerCaseName = rest.name.toLowerCase();
+    //   const lowerCaseCity = rest.city.toLowerCase();
+    //   const lowerCaseGenre = rest.genre.toLowerCase();
+
+    //   return rest.genre.includes(genre) && (rest.state === state || state === '') &&
+    //   (lowerCaseName.includes(lowerCaseSearchText) ||
+    //       lowerCaseCity.includes(lowerCaseSearchText) ||
+    //       lowerCaseGenre.includes(lowerCaseSearchText))
+    // })
+
+    // return restaurants
+  }
 
     return (
       <main className="container is-widescreen">
         <Search setSearchText={setSearchText} />
         <Filters allRestaurants={allRestaurants} setState={setState} setGenre={setGenre}/>
-        <TableSection restaurants={allRestaurants} filteredRestaurants={filteredRestaurants}/>
+        <TableSection restaurants={allRestaurants} filteredRestaurants={filteredRestaurants} 
+        isLoading={isLoading} setLoading={setLoading} />
       </main>
     )
 }
