ecfc153548784522fb70af83d2c9e4890e6b718d
Eduardo <jeduardorjx@gmail.com>
2020-10-20 17:01:06 -0600

    Updates

---
+++ precache-manifest.5625692bb85ace176553adf4512c3df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66825b4866c057dac7a6f93802171a07",
+    "url": "/.github.io/restaurant-fe/index.html"
+  },
+  {
+    "revision": "235d3236fb77a14fa58a",
+    "url": "/.github.io/restaurant-fe/static/css/main.52dfaec0.chunk.css"
+  },
+  {
+    "revision": "37d9d5075fbf696fbaa7",
+    "url": "/.github.io/restaurant-fe/static/js/2.f76e2c88.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/.github.io/restaurant-fe/static/js/2.f76e2c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235d3236fb77a14fa58a",
+    "url": "/.github.io/restaurant-fe/static/js/main.6b7e46c8.chunk.js"
+  },
+  {
+    "revision": "d8197a9674d7ded49e9c",
+    "url": "/.github.io/restaurant-fe/static/js/runtime-main.dbae35ac.js"
+  }
+]);
