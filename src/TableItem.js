9f6d73a376f51929d5dee5849a1c87e3319592e9
Eduardo <jeduardorjx@gmail.com>
2020-10-20 09:29:13 -0600

    remove default jsx for table item content

--- src/TableItem.js
+++ src/TableItem.js
@@ -58,18 +58,6 @@ export const TableItem = ({ rest }) => {
         </a>
       </header>
       {renderInfo && renderMoreInfo()}
-      {/* <div className="card-content">
-        <div className="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-          <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-          <br />
-          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        </div>
-      </div> */}
-
-      {/* <footer className="card-footer">
-        <a href="#" className="card-footer-item">Close</a>
-      </footer> */}
     </div>
   )
 }
