d1c0484c701bc0e51c4fe650d0ccdb0dc82e9faa
Eduardo <jeduardorjx@gmail.com>
2020-10-20 13:43:11 -0600

    Some code formatting with spaces and ';'

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,20 +1,20 @@
 import React, { useState , useEffect} from 'react';
 import './App.scss';
 import { getRestaurants } from '../../apiCalls';
-import { TableSection }  from '../TableSection/TableSection';
-import { restaurants } from '../../restaurants';
 import { Search } from '../../components/Search/Search';
 import { Filters } from '../../components/Filters/Filters';
+import { TableSection }  from '../TableSection/TableSection';
+import { restaurants } from '../../restaurants';
 
 
 
 export const App = () => {
   const [allRestaurants, setAllRestaurants] = useState([]);
-  const [filteredRestaurants, setFilteredRestaurants] = useState([])
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [searchText, setSearchText] = useState('');
   const [state, setState] = useState('');
   const [genre, setGenre] = useState('');
-  const [isLoading, setLoading] = useState(true)
+  const [isLoading, setLoading] = useState(true);
 
   useEffect( () => {
   //   getRestaurants()
@@ -22,9 +22,8 @@ export const App = () => {
   //   setAllRestaurants(data)
   //   setFilteredRestaurants(data)
   // })
-  
-    setAllRestaurants(restaurants)
-    setFilteredRestaurants(restaurants)
+    setAllRestaurants(restaurants);
+    setFilteredRestaurants(restaurants);
     setLoading(false)
   }, []);
 
@@ -39,14 +38,12 @@ export const App = () => {
       const lowerCaseName = rest.name.toLowerCase();
       const lowerCaseCity = rest.city.toLowerCase();
       const lowerCaseGenre = rest.genre.toLowerCase();
-
       return rest.genre.includes(genre) && (rest.state === state || state === '') &&
-      (lowerCaseName.includes(lowerCaseSearchText) ||
-          lowerCaseCity.includes(lowerCaseSearchText) ||
-          lowerCaseGenre.includes(lowerCaseSearchText))
-    })
-
-    return restaurants
+        (lowerCaseName.includes(lowerCaseSearchText) ||
+            lowerCaseCity.includes(lowerCaseSearchText) ||
+            lowerCaseGenre.includes(lowerCaseSearchText));
+    });
+    return restaurants;
   }
 
     return (
@@ -55,7 +52,7 @@ export const App = () => {
         <Filters allRestaurants={allRestaurants} setState={setState} setGenre={setGenre}/>
         <TableSection filteredRestaurants={filteredRestaurants} />
       </main>
-    )
+    );
 }
 
 export default App;
