8f854e7be4306f5b24401f13a9d564c624708863
Eduardo <jeduardorjx@gmail.com>
2020-10-20 17:05:21 -0600

    Updates

---
+++ precache-manifest.806685a8ab28869dbc9adf5d4dec65b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e41caec045892739e16d0298980efd",
+    "url": "/.github.io/restaurants-fe/index.html"
+  },
+  {
+    "revision": "c9e2ee29664a255b756d",
+    "url": "/.github.io/restaurants-fe/static/css/main.52dfaec0.chunk.css"
+  },
+  {
+    "revision": "37d9d5075fbf696fbaa7",
+    "url": "/.github.io/restaurants-fe/static/js/2.f76e2c88.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/.github.io/restaurants-fe/static/js/2.f76e2c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e2ee29664a255b756d",
+    "url": "/.github.io/restaurants-fe/static/js/main.3318e5d9.chunk.js"
+  },
+  {
+    "revision": "75f0e15b1ffbbcb884ac",
+    "url": "/.github.io/restaurants-fe/static/js/runtime-main.7368a536.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/.github.io/restaurant-fe/precache-manifest.5625692bb85ace176553adf4512c3df9.js"
+  "/.github.io/restaurants-fe/precache-manifest.806685a8ab28869dbc9adf5d4dec65b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/.github.io/restaurant-fe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/.github.io/restaurants-fe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
